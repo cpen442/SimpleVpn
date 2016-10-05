c2098838169c09d6917c20371cc72758e93491bf
Kyle Garsuta <kyle.garsuta@alumni.ubc.ca>
2016-10-05 15:04:42 -0700

    Scaffold program

---
+++ SimpleVpn/Server.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleVpn
+{
+    class Server
+    {
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
